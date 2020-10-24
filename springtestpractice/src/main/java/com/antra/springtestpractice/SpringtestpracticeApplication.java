0fa290322b7c34ea2cd8a878c062372c95c178d0
YijuanZhi <zjzyj520@gmail.com>
2020-10-24 18:11:22 -0400

    add more learning notes for spring test, forkjoinpool, webclient practice

---
+++ springtestpractice/src/main/java/com/antra/springtestpractice/SpringtestpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.antra.springtestpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtestpracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringtestpracticeApplication.class, args);
+    }
+
+}
---
+++ springtestpractice/src/test/java/com/antra/springtestpractice/SpringtestpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antra.springtestpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtestpracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
